5926f70d7dfe9015eb0cf5a63c3f1211acc704f1
Himanshu Sharma <sharmaslov@gmail.com>
2024-10-05 06:22:38 +0530

    e2e Tests

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,7 @@ import userRoutes from "./routes/users"
 import authRoutes from "./routes/auth"
 import cookieParser from "cookie-parser"
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 
 const app = express();
 app.use(cookieParser())
