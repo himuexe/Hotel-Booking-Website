f438ca044a9319acf0e660656a9e80e300994f68
Himanshu Sharma <sharmaslov@gmail.com>
2024-10-05 08:02:42 +0530

    Hotels Backend and Cloudinary Setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,16 @@ import userRoutes from "./routes/users"
 import authRoutes from "./routes/auth"
 import cookieParser from "cookie-parser"
 import path from "path"
+import {v2 as cloudinary} from "cloudinary"
+import myHotelRoutes from "./routes/my-hotels"
+
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
+})
+
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 
 const port = process.env.PORT || 7000;
@@ -23,6 +33,7 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.use("/api/auth" ,authRoutes)
 app.use("/api/users", userRoutes)
+app.use("/api/my-hotels",myHotelRoutes)
 
 app.listen(port,()=>{
     console.log("server running on localhost:7000")
