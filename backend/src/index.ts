93aef139f6a50b8b40adc26b9577ba6f2548dbcc
Himanshu Sharma <sharmaslov@gmail.com>
2024-10-05 06:39:50 +0530

    Render

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ import mongoose from 'mongoose'
 import userRoutes from "./routes/users"
 import authRoutes from "./routes/auth"
 import cookieParser from "cookie-parser"
-
+import path from "path"
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 
 const app = express();
@@ -16,6 +16,8 @@ app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
 }));
+
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.use("/api/auth" ,authRoutes)
 app.use("/api/users", userRoutes)
