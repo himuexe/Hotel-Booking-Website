e4169e31b505c446a70d3207780e9b26e3c7d854
Himanshu Sharma <sharmaslov@gmail.com>
2024-10-06 05:35:31 +0530

    Final Structure of Site Completed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,6 +13,8 @@ import MyHotels from "./pages/MyHotels";
 import EditHotel from "./pages/EditHotel";
 import Search from "./pages/Search";
 import Booking from "./pages/Booking";
+import MyBookings from "./pages/MyBookings";
+import Home from "./pages/Home";
 
 
 const App = () => {
@@ -20,7 +22,7 @@ const App = () => {
   return (
     <Router>
       <Routes>  
-        <Route path="/" element={<Layout><p>Home  Page</p></Layout>}/>
+        <Route path="/" element={<Layout><Home/></Layout>}/>
         <Route path="/search" element={<Layout><Search/></Layout>}></Route>
         <Route path="/register" element={<Layout><Register/></Layout>}></Route>
         <Route path="/sign-in" element={<Layout><SignIn/></Layout>}></Route>
@@ -28,6 +30,7 @@ const App = () => {
           <Route path="/hotel/:hotelId/booking" element={<Layout><Booking/></Layout>}/>
           <Route path="/add-hotel" element={<Layout><AddHotel/></Layout>}/>
           <Route path="/my-hotels" element={<Layout><MyHotels/></Layout>}/>
+          <Route path="/my-bookings" element={<Layout><MyBookings/></Layout>}/>
           <Route path="/edit-hotel/:hotelId" element={<Layout><EditHotel/></Layout>}/>
         </>}
 
